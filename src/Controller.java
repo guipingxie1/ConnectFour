55dc9ebea6e4e136c146276aeef94eb7365c0614
Guiping Xie <guipingxie1@gmail.com>
2016-05-21 14:37:30 -0500

    finished basic interface

---
+++ src/Controller.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author guiping
+ *
+ */
+public class Controller {
+
+  Board board;
+  
+  /* True for player one: 'X', alternates to 'Y' */
+  private boolean player;
+  
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    
+  }
+
+}
